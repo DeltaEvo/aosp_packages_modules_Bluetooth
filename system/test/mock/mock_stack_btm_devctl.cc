2ad6a9b0c1253e53b44e2d03ced996b1fd064f16
Chris Manton <cmanton@google.com>
2024-07-11 09:01:04 -0700

    mock: [49/63] Remove BTM_reset_complete
    Use get_btm_client_interface() instead
    
    Bug: 352129816
    Test: m .
    Flag: EXEMPT, Test infrastructure
    
    Change-Id: If480d15c2cbdad8afcb540bd9d886336150bbb12

--- system/test/mock/mock_stack_btm_devctl.cc
+++ system/test/mock/mock_stack_btm_devctl.cc
@@ -63,7 +63,6 @@ void BTM_VendorSpecificCommand(uint16_t /* opcode */, uint8_t /* param_len */,
 }
 void BTM_WritePageTimeout(uint16_t /* timeout */) { inc_func_call_count(__func__); }
 void BTM_db_reset(void) { inc_func_call_count(__func__); }
-void BTM_reset_complete() { inc_func_call_count(__func__); }
 void btm_delete_stored_link_key_complete(uint8_t* /* p */, uint16_t /* evt_len */) {
   inc_func_call_count(__func__);
 }
