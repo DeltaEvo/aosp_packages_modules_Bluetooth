miyu-xu/packages-modules-Bluetooth

a2dp: stop instead of disconnecting on offload fail
Instead of disconnecting AVDTP when offload fails trigger stop stream.
The offload may be failing due to ongoing SCO.

Bug: 340245529
Bug: 345709100
Flag: com.android.bluetooth.flags.stop_on_offload_fail
Test: mmm packages/modules/Bluetooth
Change-Id: I10bde20ee93efad30a472c99912262f22cb85634

system/btif/src/btif_a2dp.cc
@@ -198,11 +198,15 @@ void btif_a2dp_on_offload_started(const RawAddress& peer_addr,
   if (btif_av_is_a2dp_offload_running()) {
     if (ack != BTA_AV_SUCCESS &&
         btif_av_stream_started_ready(A2dpType::kSource)) {
-      // Offload request will return with failure from btif_av sm if
-      // suspend is triggered for remote start. Disconnect only if SoC
-      // returned failure for offload VSC
       log::error("peer {} offload start failed", peer_addr);
-      btif_av_src_disconnect_sink(peer_addr);
+      if (com::android::bluetooth::flags::stop_on_offload_fail()) {
+        btif_av_stream_stop(peer_addr);
+      } else {
+        // Offload request will return with failure from btif_av sm if
+        // suspend is triggered for remote start. Disconnect only if SoC
+        // returned failure for offload VSC
+        btif_av_src_disconnect_sink(peer_addr);
+      }
     }
   }
   if (bluetooth::audio::a2dp::is_hal_enabled()) {

DeltaEvo/aosp_packages_modules_Bluetooth

system/btif/src/btif_a2dp.cc
@@ -166,11 +166,15 @@ void btif_a2dp_on_offload_started(const RawAddress& peer_addr, tBTA_AV_STATUS st
 
   if (btif_av_is_a2dp_offload_running()) {
     if (ack != BluetoothAudioStatus::SUCCESS && btif_av_stream_started_ready(A2dpType::kSource)) {
-      // Offload request will return with failure from btif_av sm if
-      // suspend is triggered for remote start. Disconnect only if SoC
-      // returned failure for offload VSC
       log::error("peer {} offload start failed", peer_addr);
-      btif_av_source_disconnect(peer_addr);
+      if (com::android::bluetooth::flags::stop_on_offload_fail()) {
+        btif_av_stream_stop(peer_addr);
+      } else {
+        // Offload request will return with failure from btif_av sm if
+        // suspend is triggered for remote start. Disconnect only if SoC
+        // returned failure for offload VSC
+        btif_av_source_disconnect(peer_addr);
+      }
     }
   }
 