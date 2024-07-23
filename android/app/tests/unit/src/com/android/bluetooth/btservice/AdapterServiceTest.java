de91e69b8f869f1eee62644679b805399127b9ab
William Escande <wescande@google.com>
2024-07-23 16:51:11 -0700

    SystemServer: One unique method to call StopBle
    Bug: 311772251
    Test: None
    Flag: Exempt refactor
    Change-Id: I04b5b27c815d66e6a187cbdaf7b36217b7f47aed

--- android/app/tests/unit/src/com/android/bluetooth/btservice/AdapterServiceTest.java
+++ android/app/tests/unit/src/com/android/bluetooth/btservice/AdapterServiceTest.java
@@ -540,7 +540,7 @@ public class AdapterServiceTest {
 
         onToBleOn(looper, adapter, ctx, callback, onlyGatt, services);
 
-        adapter.stopBle();
+        adapter.bleOnToOff();
         TestUtils.syncHandler(looper, AdapterState.BLE_TURN_OFF);
         verifyStateChange(callback, STATE_BLE_ON, STATE_BLE_TURNING_OFF);
 
@@ -661,7 +661,7 @@ public class AdapterServiceTest {
                 false,
                 listOfMockServices());
 
-        mAdapterService.stopBle();
+        mAdapterService.bleOnToOff();
         syncHandler(AdapterState.BLE_TURN_OFF);
         verifyStateChange(STATE_BLE_ON, STATE_BLE_TURNING_OFF, CONTEXT_SWITCH_MS);
         assertThat(mAdapterService.getBluetoothGatt()).isNull();
@@ -723,7 +723,7 @@ public class AdapterServiceTest {
         assertThat(mAdapterService.getBluetoothScan()).isNotNull();
         assertThat(mAdapterService.getBluetoothGatt()).isNull();
 
-        mAdapterService.stopBle();
+        mAdapterService.bleOnToOff();
         syncHandler(AdapterState.BLE_TURN_OFF);
         verifyStateChange(callback, STATE_BLE_ON, STATE_BLE_TURNING_OFF);
 
@@ -910,7 +910,7 @@ public class AdapterServiceTest {
                 false,
                 listOfMockServices());
 
-        // Do not call stopBle().  The Adapter should turn itself off.
+        // Do not call bleOnToOff().  The Adapter should turn itself off.
         syncHandler(AdapterState.BLE_TURN_OFF);
         verifyStateChange(STATE_BLE_ON, STATE_BLE_TURNING_OFF, CONTEXT_SWITCH_MS);
 
