108b6e06bc4d73365b4d20decd2a1a792e0f7666
Kihong Seong <kihongs@google.com>
2024-08-06 09:28:36 +0000

    Add static method to convert scan mode to string for logging purposes
    Scan modes are currently logged as integers. Adding a method to convert
    them into String for better debugging.
    
    Bug: 357794209
    Test: m com.android.btservices
    Flag: EXEMPT, changes to logs only
    Change-Id: I278217e0d63ec21402abe1ae0e0b562fd7892401

--- android/app/src/com/android/bluetooth/le_scan/ScanManager.java
+++ android/app/src/com/android/bluetooth/le_scan/ScanManager.java
@@ -16,6 +16,8 @@
 
 package com.android.bluetooth.le_scan;
 
+import static android.bluetooth.le.ScanSettings.getScanModeString;
+
 import android.app.ActivityManager;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
@@ -649,7 +651,7 @@ public class ScanManager {
             Log.d(
                     TAG,
                     "Scan mode update during setAutoBatchScanClient() to "
-                            + ScanSettings.SCAN_MODE_SCREEN_OFF);
+                            + getScanModeString(ScanSettings.SCAN_MODE_SCREEN_OFF));
             if (client.stats != null) {
                 client.stats.setAutoBatchScan(client.scannerId, true);
             }
@@ -662,7 +664,8 @@ public class ScanManager {
             client.updateScanMode(client.scanModeApp);
             Log.d(
                     TAG,
-                    "Scan mode update during clearAutoBatchScanClient() to " + client.scanModeApp);
+                    "Scan mode update during clearAutoBatchScanClient() to "
+                            + getScanModeString(client.scanModeApp));
             if (client.stats != null) {
                 client.stats.setAutoBatchScan(client.scannerId, false);
             }
