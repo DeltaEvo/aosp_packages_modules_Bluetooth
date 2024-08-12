746b38a15a6bf7a06a3197e4638f7e2935a50682
Kihong Seong <kihongs@google.com>
2024-08-12 05:21:35 +0000

    Change scan mode log from integer to string
    Bug: 359082646
    Test: m com.android.btservices
    Flag: EXEMPT, changes to logs only
    Change-Id: I4bf4f96fa872499a352e7ac72e55700afd10e0c5

--- android/app/src/com/android/bluetooth/le_scan/ScanManager.java
+++ android/app/src/com/android/bluetooth/le_scan/ScanManager.java
@@ -712,9 +712,9 @@ public class ScanManager {
             Log.d(
                     TAG,
                     "Scan mode update during screen off from "
-                            + client.scanModeApp
+                            + getScanModeString(client.scanModeApp)
                             + " to "
-                            + updatedScanMode);
+                            + getScanModeString(updatedScanMode));
             return client.updateScanMode(updatedScanMode);
         }
 
@@ -828,9 +828,9 @@ public class ScanManager {
             Log.d(
                     TAG,
                     "Scan mode update during screen on from "
-                            + client.scanModeApp
+                            + getScanModeString(client.scanModeApp)
                             + " to "
-                            + getMinScanMode(scanMode, maxScanMode));
+                            + getScanModeString(getMinScanMode(scanMode, maxScanMode)));
             return client.updateScanMode(getMinScanMode(scanMode, maxScanMode));
         }
 
@@ -2104,7 +2104,7 @@ public class ScanManager {
                             + " isForeground "
                             + isForeground
                             + " scanMode "
-                            + client.settings.getScanMode());
+                            + getScanModeString(client.settings.getScanMode()));
         }
 
         if (updatedScanParams) {
